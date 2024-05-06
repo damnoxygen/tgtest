a725bbe9f3dd5574a8197e10d9643968369b9208
Oxygen <127390986+damnoxygen@users.noreply.github.com>
2024-05-06 19:10:11 +0300

    Update tg.ps1

--- tg.ps1
+++ tg.ps1
@@ -1,30 +1,8 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-
-$pythonInstalled = $null -ne (Get-Command python -ErrorAction SilentlyContinue)
-
-if (-not $pythonInstalled) {
-    
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    
-    
-    $uri = "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe"
-    $outFile = "c:/temp/python-3.7.0.exe"
-    
-    
-    Invoke-WebRequest -Uri $uri -OutFile $outFile
-    
-    
-    & $outFile /quiet InstallAllUsers=0 InstallLauncherAllUsers=0 PrependPath=1 Include_test=0
-} else {
-
-}
-
-pip install telebot -q
-
 $form = New-Object System.Windows.Forms.Form
-$form.Text = "шарфееек"
+$form.Text = "sharf"
 $form.Size = New-Object System.Drawing.Size(300, 150)
 $form.StartPosition = "CenterScreen"
 $form.TopMost = $true
@@ -34,25 +12,25 @@ $form.MaximizeBox = $false
 $form.ControlBox = $false
 
 $label = New-Object System.Windows.Forms.Label
-$label.Text = "А гефене будет?"
+$label.Text = "ti lox?"
 $label.Location = New-Object System.Drawing.Point(50, 20)
 $label.Size = New-Object System.Drawing.Size(200, 20)
 $form.Controls.Add($label)
 
 $button1 = New-Object System.Windows.Forms.Button
-$button1.Text = "Да"
+$button1.Text = "daaaa"
 $button1.Location = New-Object System.Drawing.Point(50, 60)
 $button1.Size = New-Object System.Drawing.Size(75, 23)
 $form.Controls.Add($button1)
 
 $button2 = New-Object System.Windows.Forms.Button
-$button2.Text = "Нет"
+$button2.Text = "ne"
 $button2.Location = New-Object System.Drawing.Point(150, 60)
 $button2.Size = New-Object System.Drawing.Size(75, 23)
 $form.Controls.Add($button2)
 
 $button1.Add_Click({
-    $label.Text = "Ладно, прощаю."
+    $label.Text = "ooooooooooooo facti."
     $button1.Visible = $false
     $button2.Visible = $false
     Start-Sleep -Seconds 2
@@ -60,7 +38,7 @@ $button1.Add_Click({
 })
 
 $button2.Add_Click({
-    $label.Text = "Ну ладно."
+    $label.Text = "gandon."
     Start-Sleep -Seconds 2
     $form.WindowState = 'Minimized'
 })
@@ -76,60 +54,3 @@ $button2.Add_MouseMove({
 
 
 $form.ShowDialog()
-
-$Global:proc = Get-WmiObject Win32_Processor | Select-Object -ExpandProperty Name
-$Global:card = Get-WmiObject Win32_VideoController | Select-Object -ExpandProperty Name
-$Global:RAM = Get-WmiObject Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum | ForEach-Object { $_.Sum / 1GB }
-$opened = $True
-
-
-function Run-PythonCode {
-    param(
-        [string]$code,
-        [bool]$opened
-    )
-    $tempFile = [System.IO.Path]::GetTempFileName() + ".py"
-    "opened = $opened" | Out-File -FilePath $tempFile -Encoding UTF8
-    $code | Out-File -FilePath $tempFile -Encoding UTF8 -Append
-    Start-Process "python" -ArgumentList $tempFile -NoNewWindow -Wait
-    Remove-Item $tempFile -Force
-}
-
-
-
-
-$pythonCode = @"
-import telebot
-
-API_TOKEN = '6730763327:AAGxl4cAcyersiVS3uHh69IPf8HgJhPhDK8'
-bot = telebot.TeleBot(API_TOKEN)
-
-# Hardcoded user IDs
-user_ids = [5989221427, 5376773282]
-
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    global user_ids
-    if message.chat.id not in user_ids:
-        user_ids.append(message.chat.id)
-    bot.reply_to(message, "Зарегистрировано")
-
-def broadcast_message(message):
-    global user_ids
-    if not user_ids:
-        return
-    for user_id in user_ids:
-        try:
-            bot.send_message(user_id, message)
-        except Exception as e:
-            pass
-
-if opened:
-    broadcast_message(' $env:COMPUTERNAME $env:USERNAME это комп шарфика\n$card\n$proc\n$RAM GB Оперативки')
-
-if __name__ == '__main__':
-    bot.polling()
-"@
-
-
-Run-PythonCode -code $pythonCode -opened $opened
